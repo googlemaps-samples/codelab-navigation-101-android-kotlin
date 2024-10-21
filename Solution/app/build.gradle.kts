a35673272537b7fa896e38e15eb40b8ad3c0d9ef
Ed Boiling <edboiling@google.com>
2024-10-21 15:58:29 +0100

    fix: upgrade project to Navigation SDK 6.0.0

--- Solution/app/build.gradle.kts
+++ Solution/app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
-val navSdkVersion by extra(  "5.3.0")
+val navSdkVersion by extra(  "6.0.0")
 
 android {
     namespace = "com.example.navsdkcodelab"
@@ -36,11 +37,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     buildFeatures {
         compose = true
